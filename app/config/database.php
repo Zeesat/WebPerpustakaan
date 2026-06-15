9da9fda268233ac8254793d0a72b740a9fe25ee4
Zeesat <alauddinadhaps@gmail.com>
2026-06-15 21:23:40 +0700

    refactor(database): change database config so its match host database

--- app/config/database.php
+++ app/config/database.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 return [
     'host' => '127.0.0.1',
     'port' => '3306',
-    'database' => 'library_management',
-    'username' => 'perpustakaan',
-    'password' => 'perpustakaan',
+    'database' => 'u169077025_db_libmanage',
+    'username' => 'u169077025_ale',
+    'password' => 'Perpustakaan1239',
     'charset' => 'utf8mb4',
 ];
 
